ad0bb62309d6f7060d27e19a6d51b66427dc82ca
ADMIN <ADMIN@DESKTOP-RBNQV49>
2020-05-16 21:46:17 +0700

    them file Program.cs

--- giang1/Program.cs
+++ giang1/Program.cs
@@ -10,6 +10,8 @@ namespace giang1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("nguyen hoang long giang");
+            Console.ReadLine();
         }
     }
 }
